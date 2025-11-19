78ea086731c2af4aeca52b32179e3e13867303b0
jfgordon01 <jfgordon@udel.edu>
2025-11-19 12:52:47 -0500

    added the config.h include to kernels.cu

--- pre-parallel/kernels.cu
+++ pre-parallel/kernels.cu
@@ -8,6 +8,7 @@
  *     __global__ void test_kernel(){}
  */
 #include "kernels.h"
+#include "config.h"
 
 
 __global__ void kernelForward(float* d_W1, float* d_b1, float* d_W2, float* d_b2, float* d_W3, float* d_b3, float* train_data){
