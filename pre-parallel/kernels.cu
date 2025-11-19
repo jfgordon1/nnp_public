4694feef90353be55e40f9d2f53ac413ecf234db
jfgordon01 <jfgordon@udel.edu>
2025-11-19 12:45:04 -0500

    Edited the instantiations and the bounds of the loop.

--- pre-parallel/kernels.cu
+++ pre-parallel/kernels.cu
@@ -14,22 +14,22 @@ __global__ void kernelForward(float* d_W1, float* d_b1, float* d_W2, float* d_b2
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-    float h1[H1], h1a[H1];
-    for (int j=row;j<H1;j++){
+    float h1[H1]; float h1a[H1];
+    for (int j=row; j<row+1;j++){
         h1[j]=d_b1[j];
-        for (int i=col;i<col+1;i++) h1[j]+=train_data[i]*d_W1[i*H1+j];
+        for (int i=col; i<col+1;i++) h1[j]+=train_data[i]*d_W1[i*H1+j];
         h1a[j]=relu(h1[j]);
     }
-    float h2[H2], h2a[H2];
-    for (int j=row;j<H2;j++){
+    float h2[H2]; float h2a[H2];
+    for (int j=row; j<j+1;j++){
         h2[j]=d_b2[j];
-        for (int i=col;i<H1[i]+1;i++) h2[j]+=h1a[i]*d_W2[i*H2+j];
+        for (int i=col; i<H1[i]+1;i++) h2[j]+=h1a[i]*d_W2[i*H2+j];
         h2a[j]=relu(h2[j]);
     }
-    float out[CLASSES], outa[CLASSES];
-    for (int k=row;k<CLASSES;k++){
+    float out[CLASSES]; float outa[CLASSES];
+    for (int k=row; k<k+1;k++){
         out[k]=d_b3[k];
-        for (int j=col;j<H2[k]+1;j++) out[k]+=h2a[j]*d_W3[j*CLASSES+k];
+        for (int j=col; j<H2[k]+1;j++) out[k]+=h2a[j]*d_W3[j*CLASSES+k];
     }
     softmax(out,outa,CLASSES);
 }
