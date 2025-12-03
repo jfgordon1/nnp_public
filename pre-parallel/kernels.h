f372621e119349e1ce42e2c7126758ed9748424d
jfgordon1 <149754136+jfgordon1@users.noreply.github.com>
2025-12-03 15:20:24 -0500

    Add kernel function prototypes for softmax and ReLU

--- pre-parallel/kernels.h
+++ pre-parallel/kernels.h
@@ -8,3 +8,6 @@
 
 // Kernel function prototypes
 //__global__ void test_kernel();
+__global__ void softmax(flaot* z, float* out, int len)
+__global__ inline float relu(float x)
+__global__ inline float drelu(float y)
