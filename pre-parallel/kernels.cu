b6ad6f411aedb6bc723a334967770c7fcbd1c67d
jfgordon1 <149754136+jfgordon1@users.noreply.github.com>
2025-12-03 15:18:53 -0500

    Add softmax function to kernels.cu
    Implemented softmax function for CUDA kernels.

--- pre-parallel/kernels.cu
+++ pre-parallel/kernels.cu
@@ -7,3 +7,16 @@
  *  Example:
  *     __global__ void test_kernel(){}
  */
+
+
+__global__ void softmax(flaot* z, float* out, int len){
+  float max = z[0];
+  for (int i = 1; i<len;i++) if (z[i]>max) max = z[i];
+  float sum = 0;
+  for (int i=0;i<len;i++) { out[i] = expf(z[i]-max); sum+=out[i]; }
+  for (int i=0;i<len;i++) out[i]/=sum;
+}
+
+__global__ inline float relu(float x) { return x > 0 ? x : 0; }
+
+__global__ inline float drelu(float y) { return y > 0 ? 1 : 0; }
