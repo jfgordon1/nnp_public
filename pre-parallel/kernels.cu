14cf7aab4731d8e43aa6cecfd0c6a66ba255e734
jfgordon <jfgordon@udel.edu>
2025-12-04 17:21:41 -0500

    added local variables to forwarding functions in kernels.cu so that global memory doesn't need to be accessed millions of times

--- pre-parallel/kernels.cu
+++ pre-parallel/kernels.cu
@@ -17,24 +17,30 @@ __host__ __device__ float drelu(float y) { return y > 0 ? 1 : 0; }
 __global__ void vectorMatrixMultH1(float* d_train_data, float* d_W1, float* d_b1, float* d_h1, float* d_h1a, int n) {
     int j = threadIdx.x;
     if (j >= H1) return;
-    d_h1[j]=d_b1[j];
-    for (int i=0; i<SIZE; i++) d_h1[j]+=d_train_data[n*SIZE+i]*d_W1[i*H1+j];
+    float temp = d_b1[j;]
+    //d_h1[j]=d_b1[j];
+    for (int i=0; i<SIZE; i++) temp+=d_train_data[n*SIZE+i]*d_W1[i*H1+j];
+    d_h1[j]=temp;
     d_h1a[j]=relu(d_h1[j]);
 }
 
 __global__ void vectorMatrixMultH2(float* d_h1a, float* d_W2, float* d_b2, float* d_h2, float* d_h2a) {
     int j = threadIdx.x;
     if (j >= H2) return;
-    d_h2[j]=d_b2[j];
-    for (int i=0; i<H1; i++) d_h2[j]+=d_h1a[i]*d_W2[i*H2+j];
+    float temp = d_b2[j];
+    //d_h2[j]=d_b2[j];
+    for (int i=0; i<H1; i++) temp+=d_h1a[i]*d_W2[i*H2+j];
+    d_h2[j]=temp;
     d_h2a[j]=relu(d_h2[j]);
 }
 
 __global__ void vectorMatrixMultOut(float* d_h2a, float* d_W3, float* d_b3, float* d_out, float* d_outa) {
     int j = threadIdx.x;
     if (j >= CLASSES) return;
-    d_out[j]=d_b3[j];
-    for (int i=0; i<H2; i++) d_out[j]+=d_h2a[i]*d_W3[i*CLASSES+j];
+    float temp = d_b3[j];
+    //d_out[j]=d_b3[j];
+    for (int i=0; i<H2; i++) temp+=d_h2a[i]*d_W3[i*CLASSES+j];
+    d_out[j]=temp;
     __syncthreads();
     if (j == 0) {
         float maxv = d_out[0];
